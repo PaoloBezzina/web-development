33b06e60e38d97d686892efc5f840e896e6c4756
Paolo Bezzina <paolo.bezzina.19@um.edu.mt>
2020-04-07 11:37:46 +0200

    07/04/2020 CSS update

--- homePage.php
+++ homePage.php
@@ -1,16 +1,14 @@
-<?php
-
-//insert php code here
-
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 
+<!--https://www.pngitem.com/pimgs/m/72-728254_transparent-candy-heart-png-transparent-background-white-heart.png 
+    https://cdn.clipart.email/47913fbd20856e1f998d3a75204e3fef_love-hand-drawn-heart-symbol-outline-hand-drawn-heart-clipart-_840-859.png
+    https://www.vippng.com/png/full/102-1027843_one-heart-png-transparent-png-download-white-heart.png
+-->
+
 <head>
     <link rel="stylesheet" href="styleTest.css">
+    <link rel="shortcut icon" type="image/png" href="https://www.shareicon.net/data/512x512/2016/05/25/770336_fork_512x512.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Ta' Pinu Restuarant</title>
@@ -26,11 +24,18 @@
         <li><a href="#Menu">Menu</a></li>
         <li><a href="#About Us">About Us</a></li>
         <li><a href="#Contact Us">Contact Us</a></li>
-      </ul>
+        <a class="navigation" href="#favorites">
+            <img src="https://www.vippng.com/png/full/102-1027843_one-heart-png-transparent-png-download-white-heart.png">
+        </a>
+    </ul>
+    
 </div>
 
+
+
+
 <p>Page content..</p>
-<p class="big">Note that this technique will also make the image responsive: Resize the browser window to see the effect.</p>
+<p class="scrolltest">Note that this technique will also make the image responsive: Resize the browser window to see the effect.</p>
 
 </body>
 
